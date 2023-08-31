8d7db896850fbdcf9322aa144fb6686f72938709
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2023-09-01 00:54:35 +0300

    Polished the files, removed commented out code and empty lines, added some comments

--- make_and_run_jobs.m
+++ make_and_run_jobs.m
@@ -1,19 +1,12 @@
 ind=1000;
 nojobs=220;
 
-%function_make_parallel_scripts('test_classify_tps',nojobs,ind)
-
 delete jobs/*
 delete logs/*
 
 % To run an example script
 ind=function_make_script_array(['test_script'],nojobs,ind)
 
-%ind=function_make_script_array(['perm=0;mms=''32mm'';test_classify_tps2'],nojobs,ind)
-%ind=function_make_script_array(['perm=1;mms=''32mm'';test_classify_tps2'],nojobs,ind)
-
-%ind=function_make_parallel_scripts('perm=0;test_classify_tps',nojobs,ind)
-%ind=function_make_parallel_scripts('perm=1;test_classify_tps',nojobs,ind)
 make_slurm_run_jobs
 
 system('./slurm_run_jobs_auto.sh ')
